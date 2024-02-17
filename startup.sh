dfb1a02beacf24019b5e7c4005b40be21195bbfc
Anastasiia Smirnova <anastasia.smirnova96@gmail.com>
2024-02-17 19:29:49 +0100

    correct Dockerfile

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-java -Dlogback.configurationFile=/gitbucket/logback-settings.xml -jar /opt/gitbucket.war &
-ln -s /opt/gitbucket/log/gitbucket.log /gitbucket/gitbucket.log
-sleep 100000000000000000
