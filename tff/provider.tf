6352049b96349aa35402d082e9561d663b0cb14c
Anastasiia Smirnova <anastasia.smirnova96@gmail.com>
2024-03-30 23:48:48 +0100

    set tf backend

--- tff/provider.tf
+++ tff/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "elasticbeanstalk-eu-central-1-538547716265"
+    key    = "terraform"
+    region = "eu-central-1"
+  }
+}
