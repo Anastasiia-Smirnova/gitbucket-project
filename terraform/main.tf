2683f9eb528f7c59005630d7b7cffca746fed8fe
Anastasiia Smirnova <anastasia.smirnova96@gmail.com>
2024-04-02 00:17:47 +0200

    correct format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,20 @@
 module "gitbucket_network" {
-  source                  = "./modules/network"
+  source = "./modules/network"
 }
 
 module "gitbucket_iam" {
-  source                  = "./modules/iam"
+  source = "./modules/iam"
 }
 
 module "gitbucket_db" {
-  source                  = "./modules/db"
-  db_security_group_id    = module.gitbucket_network.db_security_group_id 
-  db_subnet_01_id         = module.gitbucket_network.db_subnet_01_id
-  db_subnet_02_id         = module.gitbucket_network.db_subnet_02_id
+  source               = "./modules/db"
+  db_security_group_id = module.gitbucket_network.db_security_group_id
+  db_subnet_01_id      = module.gitbucket_network.db_subnet_01_id
+  db_subnet_02_id      = module.gitbucket_network.db_subnet_02_id
 }
 
 module "gitbucket_app" {
-  source                  = "./modules/app"
+  source = "./modules/app"
 
   instance_profile_name   = module.gitbucket_iam.app_instance_profile
   vpc_id                  = module.gitbucket_network.vpc_id
